3e122690cab4f31bf5d033103485a48254ded748
Wesley Finck <wesleyfinck@gmail.com>
2022-07-19 20:46:35 +0200

    exercise 2 step 6 passing

--- dnas/private_publication/integrity_zomes/private_publication/src/post.rs
+++ dnas/private_publication/integrity_zomes/private_publication/src/post.rs
@@ -1,5 +1,6 @@
 use hdi::prelude::*;
 use crate::{properties::progenitor, *};
+use crate::publication_role::PublicationRole;
 
 #[hdk_entry_helper]
 pub struct Post {
@@ -8,7 +9,19 @@ pub struct Post {
 }
 
 pub fn validate_create_post(action: EntryCreationAction) -> ExternResult<ValidateCallbackResult> {
-    Ok(ValidateCallbackResult::Valid)
+    // Ok(ValidateCallbackResult::Valid)
+    let author = action.author().clone();
+    if author == progenitor()? {
+        Ok(ValidateCallbackResult::Valid)
+    }
+    else {
+        let publication_role = PublicationRole {
+            role: String::from("editor"),
+            assignee: author,
+        };
+        must_get_entry(hash_entry(publication_role)?)?;
+        Ok(ValidateCallbackResult::Valid)
+    }
 }
 
 pub fn validate_update_post(
